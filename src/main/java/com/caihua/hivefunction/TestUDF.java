97991fbe385bae8b364ff9d9919bca369a9e3320
夕林石山 <2635203928@qq.com>
2019-11-30 21:14:02 +0800

    Hive学习代码1.0

---
+++ src/main/java/com/caihua/hivefunction/TestUDF.java
@@ -0,0 +1,16 @@
+package com.caihua.hivefunction;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * @author XiLinShiShan
+ * @version 0.0.1
+ */
+public class TestUDF extends UDF {
+    public String evaluate(String str){
+        if(str.equals("a")){
+            return "_";
+        }
+        return null;
+    }
+}
